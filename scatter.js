6f37d36b35dbd8d44bd95a1719a0e2763e3f4bc1
Shashank Kashyap <50551759+SKVKPandey@users.noreply.github.com>
2022-03-24 11:03:47 +0530

    Zooming of Axis Added

--- scatter.js
+++ scatter.js
@@ -26,15 +26,19 @@ d3.csv('geant4.csv', function (data) {
     var inst = [];
     var branch = [];
     var miss = [];
+    var cpi = [];
+    var bmf = [];
 
     for (let i = 0; i<data.length; i++) {
         cyc.push(data[i]['cycles']);
         inst.push(data[i]['instructions']);
         branch.push(data[i]['branches']);
         miss.push(data[i]['branchmisses']);
+        cpi.push(data[i]['cpi']);
+        bmf.push(data[i]['bmf']);
     };  
 
-    var DataSet = { 'cycles': cyc, 'instructions': inst, 'branches': branch, 'branchmisses': miss};
+    var DataSet = { 'cycles': cyc, 'instructions': inst, 'branches': branch, 'branchmisses': miss, 'cpi': cpi, 'bmf': bmf};
 
     // Function for Determining Max Values
     function roundtoMax(arr1, arr2) {
@@ -45,12 +49,18 @@ d3.csv('geant4.csv', function (data) {
 
     // Default X-Axis, Y-Axis and Radius
     d3.select("#X[value=\"option1\"]").property("checked", true);
-    d3.select("#Y[value=\"option6\"]").property("checked", true);
+    d3.select("#Y[value=\"option8\"]").property("checked", true);
     d3.select("#R[value=\"optionA\"]").property("checked", true);
+    d3.select("#XZ[value=\"optionX\"]").property("value", 1);
+    d3.select("#YZ[value=\"optionY\"]").property("value", 1);
 
     var data1 = 'cycles';
     var data2 = 'instructions';
     var radius = 'cycles';
+    var xdiv = document.getElementById('XZ').value;
+    var ydiv = document.getElementById('YZ').value;
+    var xslider = document.getElementById('XZ');
+    var yslider = document.getElementById('YZ');
     
     // Setting the Default Plot
     // Scales
@@ -58,14 +68,14 @@ d3.csv('geant4.csv', function (data) {
     var xScale = d3.scale.linear()
       .domain([
           0,
-          roundtoMax(DataSet[data1], DataSet[data2])
+          roundtoMax(DataSet[data1], DataSet[data2])/xdiv
           ])
       .range([0,w])
       
     var yScale = d3.scale.linear()
       .domain([
           0,
-          roundtoMax(DataSet[data2], DataSet[data1])
+          roundtoMax(DataSet[data2], DataSet[data1])/ydiv
           ])
       .range([h,0])
 
@@ -92,26 +102,29 @@ d3.csv('geant4.csv', function (data) {
       .append('circle')
         .attr('cx',function (d) { return xScale(d[data1]) })
         .attr('cy',function (d) { return yScale(d[data2]) })
-        .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15 })
+        .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15*((0.5*xdiv)+(0.5*ydiv)) })
         .attr('stroke','black')
+        .style('opacity', 0.9)
         .attr('stroke-width',1)
         .attr('fill',function (d) {  return colorScale(libs.indexOf(d.dso)) })
         .on('mouseover', function () {
           d3.select(this)
             .transition()
             .duration(500)
-            .attr('r',function (d) { return 20 })
-            .attr('stroke-width',3)
+            .attr('r',function (d) { if ((d[radius]/(Math.max.apply(null,DataSet[radius])))*15*((0.5*xdiv)+(0.5*ydiv))<20){return 20} else {return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15*((0.5*xdiv)+(0.5*ydiv))} })
+            .attr('stroke-width',2)
+            .style('opacity', 0.8)
         })
         .on('mouseout', function () {
           d3.select(this)
             .transition()
             .duration(500)
-            .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15 })
+            .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15*((0.5*xdiv)+(0.5*ydiv)) })
             .attr('stroke-width',1)
+            .style('opacity', 0.9)
         })
         .append('title') // Tooltip
-        .text(function (d) { return 'Library: ' + d.dso + '\nSymbols: ' + d.symbol + '\nCycles: ' + d.cycles + '\nInstructions: ' + d.instructions + '\nBranches: ' + d.branches + '\nBranch-Misses: ' + d.branchmisses});
+        .text(function (d) { return 'Library: ' + d.dso + '\nSymbols: ' + d.symbol + '\nCycles: ' + d.cycles + '\nInstructions: ' + d.instructions + '\nBranches: ' + d.branches + '\nBranch-Misses: ' + d.branchmisses + '\nCPI: ' + d.cpi + '\nBranch-Miss Factor: ' + d.bmf});
 
         
 
@@ -143,7 +156,6 @@ d3.csv('geant4.csv', function (data) {
         .style('text-anchor','end')
         .text(data2.charAt(0).toUpperCase() + data2.slice(1));
 
-
     d3.selectAll("input")
 	    .on("change", selectDataset);
 	
@@ -153,81 +165,121 @@ d3.csv('geant4.csv', function (data) {
 	    if (value == "option1")
 	    {
         data1 = 'cycles';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
 	    else if (value == "option2")
 	    {
 		    data1 = 'instructions';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
         else if (value == "option3")
 	    {
 		    data1 = 'branches';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "option4")
 	    {
 		    data1 = 'branchmisses';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "option5")
 	    {
-		    data2 = 'cycles';
-		    change(data1, data2);
+		    data1 = 'cpi';
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "option6")
 	    {
-		    data2 = 'instructions';
-		    change(data1, data2);
+		    data1 = 'bmf';
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "option7")
 	    {
-		    data2 = 'branches';
-		    change(data1, data2);
+		    data2 = 'cycles';
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "option8")
 	    {
+		    data2 = 'instructions';
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "option9")
+	    {
+		    data2 = 'branches';
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "option10")
+	    {
 		    data2 = 'branchmisses';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "option11")
+	    {
+		    data2 = 'cpi';
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "option12")
+	    {
+		    data2 = 'bmf';
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "optionA")
 	    {
 		    radius = 'cycles';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "optionB")
 	    {
 		    radius = 'instructions';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "optionC")
 	    {
 		    radius = 'branches';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
 	    }
       else if (value == "optionD")
 	    {
 		    radius = 'branchmisses';
-		    change(data1, data2);
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "optionE")
+	    {
+		    radius = 'cpi';
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (value == "optionF")
+	    {
+		    radius = 'bmf';
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (xslider.value!=xdiv)
+	    {
+        xdiv = xslider.value;
+		    change(data1, data2, xdiv, ydiv);
+	    }
+      else if (yslider.value!=ydiv)
+	    {
+        ydiv = yslider.value;
+		    change(data1, data2, xdiv, ydiv);
 	    }
     }
 
 
-function change(data1, data2) {
+function change(data1, data2, xdiv, ydiv) {
 
     // Update scale domains
     var colorScale = d3.scale.category20()
     var xScale = d3.scale.linear()
       .domain([
           0,
-          roundtoMax(DataSet[data1], DataSet[data2])
+          roundtoMax(DataSet[data1], DataSet[data2])/xdiv
           ])
       .range([0,w]);
     
     var yScale = d3.scale.linear()
       .domain([
           0,
-          roundtoMax(DataSet[data2], DataSet[data1])
+          roundtoMax(DataSet[data2], DataSet[data1])/ydiv
           ])
       .range([h,0]);
 
@@ -281,7 +333,7 @@ function change(data1, data2) {
       //.ease("linear")  // Transition easing - default 'variable' (i.e. has acceleration), also: 'circle', 'elastic', 'bounce', 'linear'
       .attr('cx',function (d) { return xScale(d[data1]) })
         .attr('cy',function (d) { return yScale(d[data2]) })
-        .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15 }) // Circle's Y
+        .attr('r',function (d) { return (d[radius]/(Math.max.apply(null,DataSet[radius])))*15*((0.5*xdiv)+(0.5*ydiv)) }) // Circle's Y
       .each("end", function() {  // End animation
           d3.select(this)  // 'this' means the current element
               .transition()
